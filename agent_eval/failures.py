fde37af74c16765e4a12d48042db25883f1229d6
HanW-ai <wanghanxuan0420@gmail.com>
2026-06-02 17:09:04 -0700

    Create failures.py

---
+++ agent_eval/failures.py
@@ -0,0 +1,9 @@
+FAILURE_CATEGORIES = [
+    "tool_misuse",
+    "context_loss",
+    "hallucinated_action",
+    "incomplete_task",
+    "incorrect_reasoning",
+    "format_error",
+    "success"
+]
